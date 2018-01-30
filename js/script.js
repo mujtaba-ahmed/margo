1ece30bf09cd4e1ee65c161647339240c0be5917
mujtaba-ahmed <mujtaba.programmer@gmail.com>
2018-01-30 11:59:43 +0500

    This is my fourteen repositorie

---
+++ js/script.js
@@ -0,0 +1,226 @@
+// Page Loader
+$(window).load(function () {
+    "use strict";    
+	$('#loader').fadeOut();
+});
+
+$(document).ready(function ($) {
+	"use strict";	
+	////	Hidder Header	
+	var headerEle = function () {
+		var $headerHeight = $('header').height();
+		$('.hidden-header').css({ 'height' : $headerHeight  + "px" });
+	};
+	
+	$(window).load(function () {
+	    headerEle();
+	});
+	
+	$(window).resize(function () {
+	    headerEle();
+	});  	  
+    /*---------------------------------------------------*/
+    /* Progress Bar
+    /*---------------------------------------------------*/    
+    $('.skill-shortcode').appear(function() {
+  		$('.progress').each(function(){ 
+    		$('.progress-bar').css('width',  function(){ 
+    			return ($(this).attr('data-percentage')+'%')});
+  		});
+	},{accY: -100});	
+	/*----------------------------------------------------*/
+	/*	Sliders & Carousel
+	/*----------------------------------------------------*/
+	////------- Touch Slider
+	var time = 4.4,
+		$progressBar,
+		$bar,
+		$elem,
+		isPause,
+		tick,
+		percentTime;
+	$('.touch-slider').each(function(){
+		var owl = jQuery(this),
+			sliderNav = $(this).attr('data-slider-navigation'),
+			sliderPag = $(this).attr('data-slider-pagination'),
+			sliderProgressBar = $(this).attr('data-slider-progress-bar');
+			
+		if ( sliderNav == 'false' || sliderNav == '0' ) {
+			var returnSliderNav = false
+		}else {
+			var returnSliderNav = true
+		}
+		
+		if ( sliderPag == 'true' || sliderPag == '1' ) {
+			var returnSliderPag = true
+		}else {
+			var returnSliderPag = false
+		}
+		
+		if ( sliderProgressBar == 'true' || sliderProgressBar == '1' ) {
+			var returnSliderProgressBar = progressBar
+			var returnAutoPlay = false
+		}else {
+			var returnSliderProgressBar = false
+			var returnAutoPlay = true
+		}
+		
+		owl.owlCarousel({
+			navigation : returnSliderNav,
+			pagination: returnSliderPag,
+			slideSpeed : 400,
+			paginationSpeed : 400,
+			lazyLoad : true,
+			singleItem: true,
+			autoHeight : true,
+			autoPlay: returnAutoPlay,
+			stopOnHover: returnAutoPlay,
+			transitionStyle : "fade",
+			afterInit : returnSliderProgressBar,
+			startDragging : pauseOnDragging
+		});
+		
+	});
+
+    function progressBar(elem){
+		$elem = elem;
+		buildProgressBar();
+		start();
+    }
+	
+    function buildProgressBar(){
+		$progressBar = $("<div>",{
+			id:"progressBar"
+		});
+		$bar = $("<div>",{
+			id:"bar"
+		});
+		$progressBar.append($bar).prependTo($elem);
+    }
+	
+    function pauseOnDragging(){
+      isPause = true;
+    }
+	/*----------------------------------------------------*/
+	/*	Animation Progress Bars
+	/*----------------------------------------------------*/
+	$("[data-progress-animation]").each(function() {
+		
+		var $this = $(this);
+		
+		$this.appear(function() {
+			
+			var delay = ($this.attr("data-appear-animation-delay") ? $this.attr("data-appear-animation-delay") : 1);
+			
+			if(delay > 1) $this.css("animation-delay", delay + "ms");
+			
+			setTimeout(function() { $this.animate({width: $this.attr("data-progress-animation")}, 800);}, delay);
+
+		}, {accX: 0, accY: -50});
+
+	});
+	/*----------------------------------------------------*/
+	/*	Sticky Header
+	/*----------------------------------------------------*/
+	
+	(function() {
+		
+		var docElem = document.documentElement,
+			didScroll = false,
+			changeHeaderOn = 100;
+			document.querySelector( 'header' );
+			
+		function init() {
+			window.addEventListener( 'scroll', function() {
+				if( !didScroll ) {
+					didScroll = true;
+				}
+			}, false );
+		}
+		
+				function scrollY() {
+			return window.pageYOffset || docElem.scrollTop;
+		}
+		
+		init();		
+	})();
+});
+// Styles Switcher JS
+function setActiveStyleSheet(title) {
+  var i, a, main;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+      a.disabled = true;
+      if(a.getAttribute("title") == title) a.disabled = false;
+    }
+  }
+}
+
+function getActiveStyleSheet() {
+  var i, a;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
+  }
+  return null;
+}
+
+function getPreferredStyleSheet() {
+  var i, a;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1
+       && a.getAttribute("rel").indexOf("alt") == -1
+       && a.getAttribute("title")
+       ) return a.getAttribute("title");
+  }
+  return null;
+}
+
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+window.onload = function(e) {
+  var cookie = readCookie("style");
+  var title = cookie ? cookie : getPreferredStyleSheet();
+  setActiveStyleSheet(title);
+}
+
+window.onunload = function(e) {
+  var title = getActiveStyleSheet();
+  createCookie("style", title, 365);
+}
+
+var cookie = readCookie("style");
+var title = cookie ? cookie : getPreferredStyleSheet();
+setActiveStyleSheet(title);
+
+$(document).ready(function(){	
+	//Background Switcher
+	$('.switcher-box .bg-list li a').click(function() {
+		var current = $('.switcher-box select[id=layout-style]').find('option:selected').val();
+		if(current == '2') {
+			var bg = $(this).css("backgroundImage");
+			$("body").css("backgroundImage",bg);
+		} else {
+			alert('Please select boxed layout');
+		}
+	});
+
+});
